4be2811724bd8762da2c1c12e2b0e7f5d9c89dee
Naman1Agarwal <m270036@usna.edu>
2025-09-12 13:53:35 -0400

    update

--- Lab03/README.txt
+++ Lab03/README.txt
@@ -3,5 +3,5 @@ Length: 2 hours
 Suggestions: None
 Comments: None
 Documentation: I believe my webpage has all the components listed in
-part1 and part0.
-AI citations: I used google search which gives the ai overview to see how to do some of the css formatting. I used ai to understand 
+part1 and part2.
+AI citations: I used google search to see how to embed html in my php script. I also figured out how to do the majority of my css for my registration page with ai but did not do any copy and paste.
--- Lab03/submit.php
+++ Lab03/submit.php
@@ -4,7 +4,7 @@ $player_email = $_POST['Player_Email'] ?? '';
 $player_opponent = $_POST['Opponent'] ?? '';
 $playing_match = $_POST['for_match'] ?? '';
 $exp_level = $_POST['experience'] ?? '';
-
+$player_password = $_POST['Player_Password'] ?? '';
 
 $valid_input = true;
 $missing = "";
@@ -25,40 +25,67 @@ if (empty($exp_level)){
   $valid_input = false;
   $missing = $missing . "Experience Level\n";
 }
-
+if (empty($player_password)){
+  $valid_input = false;
+  $missing = $missing . "Password\n";
+}
 
 if ($valid_input == False){ ?>
 
-    <!DOCTYPE html>
+  <!DOCTYPE html>
     <html lang="en">
     <head> <title> Invalid Form </title> </head>
     <body>
-        <h3> <center> Your form is not fully completed! </center> </h1>
-        <p> <center> <b> You are missing the following: </b> </center> </p>
-        <p> <center> <?php echo nl2br(htmlspecialchars($missing)); ?></center> </p>
+    <h3> <center> Your form is not fully completed! </center> </h1>
+    <p> <center> <b> You are missing the following: </b> </center> </p>
+    <p> <center> <?php echo nl2br(htmlspecialchars($missing)); ?></center> </p>
+    <p> <center> Please go back to complete your registration! </center> </p>
     </body>
     </html>
-<?php } else { ?>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head> <title> Registration Form </title> </head>
-    <body>
+    <?php } else { ?>
+      <!DOCTYPE html>
+        <html lang="en">
+        <head> <title> Registration Form </title> </head>
+        <body>
         <h3> <center> Your registration to play has been confirmed! </center> </h1>
         <p> <center> <b> The details for when you play are:  </b> </center> </p>
         <p> <center> Your name: <?php echo htmlspecialchars($pname); ?> </center> </p>
         <p> <center> Your email: <?php echo htmlspecialchars($player_email); ?> </center> </p>
+
+        <?php 
+        if (empty($player_opponent)){
+          $player_opponent = "Naman";
+        }
+      ?>
+
         <p> <center> Your opponent: <?php echo htmlspecialchars($player_opponent); ?> </center> </p>
-         <?php 
-         if ($playing_match == 'one') { 
-         ?>
-           <p> <center> You are playing a match </center> </p>
-         <?php } else {?>
-           <p> <center> You are not playing a match </center> </p>
-         <?php } ?>
-         
-         <p> <center> Your playing level is <?php echo htmlspecialchars($exp_level); ?>! </center> </p>
-         <p> <center> Make sure you remember your password: ****</center> </p>
-          
-    </body>
-    </html>
-<?php } ?>
+        <?php 
+        if ($playing_match == 'one') { 
+          ?>
+            <p> <center> You are playing a match </center> </p>
+            <?php } else {?>
+              <p> <center> You are not playing a match </center> </p>
+                <?php } ?>
+
+                <p> <center> Your playing level is <?php echo htmlspecialchars($exp_level); ?>! </center> </p>
+                <p> <center> Make sure you remember your password: ****</center> </p>
+
+                </body>
+                </html>
+
+                <?php
+                $file = fopen("LOG.txt", "a+");
+      $line = "";
+      $line = $line . $pname . "\t";
+      $line = $line . $player_email . "\t";
+      $line = $line . $playing_match . "\t";
+      $line = $line . $player_opponent . "\t";
+      $line = $line . $exp_level . "\t";
+      $line = $line . $player_password . "\t";
+      $line = $line . "\n";
+
+      fwrite($file, $line);
+      fclose($file);
+      ?>
+
+        <?php } ?>
